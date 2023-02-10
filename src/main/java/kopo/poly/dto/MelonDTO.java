abfc5fc1d89bf9c3d1275702b4aa03bf07bb051b
hyegyeong25 <hyegyeong25@gmail.com>
2023-02-10 16:30:51 +0900

    23.02.10 멜론기초실습

---
+++ src/main/java/kopo/poly/dto/MelonDTO.java
@@ -0,0 +1,14 @@
+package kopo.poly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Data
+public class MelonDTO {
+    String collectTime;
+    String song;
+    String singer;
+    int singerCnt;
+
+}
