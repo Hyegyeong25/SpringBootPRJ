3794abc9f35b898a3355641c5c59f6d323dc74f5
hyegyeong25 <hyegyeong25@gmail.com>
2023-02-14 07:25:24 +0900

    23.02.14 멜론기초실습

---
+++ src/main/java/kopo/poly/persistance/mongodb/impl/MelonMapper.java
@@ -0,0 +1,10 @@
+package kopo.poly.persistance.mongodb.impl;
+
+import kopo.poly.persistance.mongodb.AbstractMongoDBComon;
+import kopo.poly.persistance.mongodb.IMelonMapper;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+
+public class MelonMapper extends AbstractMongoDBComon implements IMelonMapper {
+    //private final MongoTemplate mongodb;
+}
